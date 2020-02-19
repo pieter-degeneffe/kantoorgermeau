6033dcee9f4cc5b07393e85718595bef796d04a0
Pieter Degeneffe <pieterdegeneffe@Pieters-MacBook-Pro.local>
2020-02-19 11:09:57 +0100

    Updated website

---
+++ assurmifid.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Crelan Kantoor Germeau</title>
+    <link rel="stylesheet" href="css/master.css">
+    <link rel="icon" type="image/png" sizes="32x32" href="favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="favicon-16x16.png">
+  </head>
+  <body>
+    <?php require_once('php/header.php') ?>
+    <section id="main">
+      <div class="container">
+        <td width="616" id="inhoud"><!-- InstanceBeginEditable name="Inhoud subtitel" -->
+      <h3>AssurMifid - Belangenconflictenbeleid</h3>
+      <h5>Wetgevend kader</h5>
+<p>Vanaf 30 april 2014 zijn de “<em>AssurMiFID –gedragsregels”</em> van kracht. Zij vinden hun wettelijke  basis in de wet van 30 juli 2013 tot versterking van de bescherming van de  afnemers van financiële producten en diensten alsook van de bevoegdheden van de  FSMA en diverse bepalingen alsook het KB van 21 februari 2014 over de regels  voor de toepassing van de artikelen 27 tot 28bis van de wet van 2 augustus 2002  betreffende het toezicht op de financiële sector en de financiële diensten op  de verzekeringssector en KB van 21 februari 2014 inzake de krachtens de wet  vastgestelde gedragsregels en regels over het beheer van belangenconflicten,  wat de verzekeringssector betreft.</p>
+      <p>Overeenkomstig deze gedragsregels is ons kantoor gehouden  om een schriftelijk beleid op te stellen voor het beheer van belangenconflicten  bij het verlenen van diensten van verzekeringsbemiddeling.</p>
+      <p>De wettelijke regeling omtrent belangenconflicten is  een aanvulling op de algemene MiFID-grondwet. Deze grondwet wordt door ons  kantoor geëerbiedigd door zich lo<strong>yaal, billijk en professioneel in te zetten  voor de belangen van de klant bij het verlenen van diensten van  verzekeringsbemiddeling</strong>. </p>
+      <h5>Welke belangenconflicten</h5>
+      <p>Met het oog op ons belangenconflictenbeleid heeft ons  kantoor in een eerste stap de mogelijke belangenconflicten in ons kantoor  geïdentificeerd. </p>
+      <p> Belangenconflicten kunnen zich voordoen tussen (1) ons  kantoor en haar verbonden personen en een klant of (2) tussen meerdere klanten onderling.  Het beleid inzake belangenconflicten houdt rekening met de eigen kenmerken van  ons kantoor en haar eventuele groepsstructuur.</p>
+      <p> Bij de beoordeling naar mogelijke belangenconflicten heeft  ons kantoor de situaties in kaart gebracht waarbij een aanzienlijk risico bestaat  dat de belangen van de klant worden geschaad. Het gaat om:</p>
+      <ul>
+        <li>Situaties waarbij winst wordt gemaakt of  verlies wordt geleden ten koste van de cliënt;</li>
+        <li>Situaties waarbij ons kantoor een ander  belang heeft bij het resultaat van de dienst of transactie;</li>
+        <li>Situaties met een financiële drijfveer om  andere cliënten te laten voorgaan;</li>
+        <li>Situaties waarbij hetzelfde bedrijf als de  cliënt wordt uitgeoefend;</li>
+        <li>Situaties waarbij ons kantoor een  vergoeding ontvangt van een andere persoon dan de cliënt voor de geleverde  diensten van verzekeringsbemiddeling.</li>
+        <li>Situaties waarbij ons kantoor deelnemingen  van minstens 10% bezit in stemrechten of van het kapitaal van de verzekeringsonderneming(en);</li>
+        <li>Situaties waarbij de verzekeringsonderneming(en)  deelnemingen van minstens 10% bezit(ten)van de stemrechten of van het kapitaal  van ons kantoor;</li>
+        <li>Situaties  eigen aan ons kantoor als bankagent van Crelan.</li>
+      </ul>
+      <h5>Welke maatregelen neemt ons kantoor</h5>
+<p>Ons kantoor neemt heel wat  maatregelen om ervoor te zorgen dat het belang van de klant primeert.<br>
+Het gaat onder meer om:</p>
+<ol>
+  <li>Een interne instructienota;</li>
+  <li>Een aangepast verloningsbeleid;</li>
+  <li>Een beleid dat erop toeziet dat de  verbonden personen enkel bemiddelen m.b.t. verzekeringsovereenkomsten waarvan  zij de essentiële kenmerken kennen en in staat zijn om aan de cliënten toe te  lichten; </li>
+  <li>Een beleid dat het recht voorbehoudt van  ons kantoor om bij ontstentenis van een concrete oplossing voor een specifiek  belangenconflict de gevraagde dienstverlening te weigeren met het uitsluitende  doel de bescherming van de belangen van de klant; </li>
+  <li>Een regeling inzake het ontvangen van  voordelen;</li>
+  <li>Een beleid dat erop toeziet dat alle  informatie die onze verbonden personen verstrekken correct, duidelijk en niet  misleidend is.</li>
+</ol>
+<p>Indien noodzakelijk zal het beleid inzake belangenconflicten van ons  kantoor aangepast en/of geactualiseerd worden.</p>
+<h5>Wat is de procedure</h5>
+      <p><img src="belangenconflictenbeleid-beslissingsboom.jpg" width="609" height="200" alt="belangenconflictenbeleid stroomschema"></p>
+      <h5>Specifieke transparantie</h5>
+      <p>Wanneer in een concrete  situatie onze maatregelen onvoldoende garantie zouden kunnen bieden, zal u geïnformeerd  worden door ons kantoor over de algemene aard en/of de bronnen van het  belangenconflict, zodat u een weloverwogen beslissing kan nemen. U kan steeds  met ons contact opnemen voor meer informatie.</p>
+      <p>&nbsp;</p>
+      <p>laatste aanpassing: 01.05.2014</p>
+    <!-- InstanceEndEditable --></td>
+      </div>
+    </section>
+    <?php require_once('php/footer.php') ?>
+    <script src="js/main.js"></script>
+  </body>
+</html>
---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Crelan Kantoor Germeau</title>
+    <link rel="stylesheet" href="css/master.css">
+    <link rel="icon" type="image/png" sizes="32x32" href="favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="favicon-16x16.png">
+  </head>
+  <body>
+    <?php require_once('php/header.php') ?>
+    <section id="main">
+      <div class="container">
+        <h2>Welkom bij Crelan kantoor Germeau</h2>
+        <p>We zijn een zelfstandig Crelan bankkantoor & onafhankelijk verzekeringsmakelaar die zowel voor particulieren als bedrijven een totaalservice aanbiedt.  Met meer dan 30 jaar ervaring hebben wij de nodige knowhow opgebouwd om u de beste spaar,- beleggings, -krediet- en verzekeringsformules aan te bieden.  Als zelfstandig kantoorhouder staan we voor een clientgerichte, persoonlijke service.</p>
+        <p>We verwelkomen u graag in ons kantoor!</p>
+      </div>
+    </section>
+    <?php require_once('php/footer.php') ?>
+    <script src="js/main.js"></script>
+  </body>
+</html>
