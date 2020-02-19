d418c58fc4d001332ea3fdc3fad5488b24d38519
Pieter Degeneffe <pieterdegeneffe@Pieters-MacBook-Pro.local>
2020-02-19 11:09:43 +0100

    Placed header and footer in seperate php files

---
+++ php/footer.php
@@ -0,0 +1,49 @@
+<section id="pre-footer">
+  <div class="container">
+    <div id="col-1-2-wrapper">
+      <div class="col-1-2">
+        <h3>Openingsuren</h3>
+        <table id="openingsuren">
+          <tr>
+            <td>Maandag</td>
+            <td>09:00 tot 12:00 en 16:00 tot 18:00
+          </tr>
+          <tr>
+            <td>Dinsdag</td>
+            <td>09:00 tot 12:00 en 16:00 tot 18:00
+          </tr>
+          <tr>
+            <td>Woensdag</td>
+            <td>09:00 tot 12:00 en 16:00 tot 18:00
+          </tr>
+          <tr>
+            <td>Donderdag</td>
+            <td>09:00 tot 12:00 en 16:00 tot 18:00
+          </tr>
+          <tr>
+            <td>Vrijdag</td>
+            <td>09:00 tot 12:00 en 16:00 tot 18:00
+          </tr>
+          <tr>
+            <td>Zaterdag</td>
+            <td>09:00 tot 12:00
+          </tr>
+        </table>
+      </div>
+      <div class="col-1-2">
+        <h3>Contactgegevens</h3>
+        <ul id="contactgegevens">
+          <li>Gorsemweg 84, 3800 Sint-Truiden</li>
+          <li>Tel: +32 11/687371</li>
+          <li>Fax: +32 11/687286</li>
+          <li>E-mail:	sinttruiden.gorsemweg@crelan.be</li>
+        </ul>
+      </div>
+    </div>
+  </div>
+</section>
+<footer>
+  <div class="container">
+    <p id="copyright">© 2020 Crelan Kantoor Germeau. Alle rechten voorbehouden. Website ontwikkeld door <a href="https://design8.be">DESIGN8</a>
+  </div>
+</footer>
---
+++ php/header.php
@@ -0,0 +1,16 @@
+<header>
+  <div class="container">
+    <a href="index.php" title="Kantoor Germeau" id="logo">
+      <img src="images/logo.svg" alt="Kantoor Germeau" title="Kantoor Germeau" class="logo">
+    </a>
+    <nav>
+      <ul id="site-nav">
+        <li><a href="index.php">Home</a></li>
+        <li><a href="assurmifid.php">Belangenconflictenbeleid</a><li>
+        <li class="icon">
+          <a href="javascript:void(0);" onclick="responsiveMenu()">☰</a>
+        </li>
+      </ul>
+    </nav>
+  </div>
+</header>
